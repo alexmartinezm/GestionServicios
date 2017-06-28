3f76b2861b6c76a18b33b3d991b0573db9cab5fc
amartinez <amartinez@gespol.com>
2017-06-28 12:12:32 +0200

    Init commit

---
+++ GestionServicios/GestionServicios/GestionServicios.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace GestionServicios.Droid
+{
+    [Activity(MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
